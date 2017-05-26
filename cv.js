ad5218592732d724dd3eaf3220eded1793014aa3
Tommy Gunnarsson <tommy.gunnarsson@dektech.com.au>
2017-05-26 20:44:33 +0200

    Fixed parsing of cv.

--- cv.js
+++ cv.js
@@ -1,45 +1,65 @@
-function getAsCollapsible(title, content) {
+function buildCollapsible(title, content, index) {
 	var collapsible = document.getElementById('collapsible-group').innerText;
-	collapsible.replace("TITLE", title);
-	collapsible.replace("CONTENT", content);
+	collapsible = collapsible.replace("TITLE", title);
+	collapsible = collapsible.replace("CONTENT", content);
+	collapsible = collapsible.replace("INDEX", index);
+	collapsible = collapsible.replace("INDEX", index);
 	return collapsible;
+}
+
+function formatTitle(title) {
+	return "<b>" + title + "</b>";
+}
+
+function formatContent(content) {
+	return content;
 }
 
 function getAllCollapsibles(fileContent) {
 	var collapsibles = "";
 	var title = "";
 	var content = "";
-	
-	for (var lineNr = 0; lineNr < fileContent.length; ++lineNr) {	
-		if (fileContent[lineNr].substr(0, 1) == "H") {
-			title = fileContent[lineNr].substr(2);
+	var headingIndex = 0;
+	var lines = fileContent.split("\n");
+
+	for (var lineNr = 0; lineNr < lines.length; ++lineNr) {
+		if (lines[lineNr].substr(0, 1) == "H") {
+			++headingIndex;
+			title = lines[lineNr].substr(2);
 		}
-		else if (fileContent[lineNr].substr(0, 1) == "B") {
-			content += fileContent[lineNr].substr(2);
-			content += "\n";
+		else if (lines[lineNr].substr(0, 1) == "B") {
+			content += lines[lineNr].substr(2);
 		}
-		else if (fileContent[lineNr].substr(0, 1) == "E") {
-			collapsibles += getAsCollapsible(title, content);
+		else if (lines[lineNr].substr(0, 1) == "E") {
+			collapsibles += buildCollapsible(formatTitle(title),
+													formatContent(content),
+													headingIndex);
 			title = "";
 			content = "";
 		}
 	}
-
+	alert(collapsibles);
 	return collapsibles;
 }
 
-function updateDivContent(fileContent) {	
-	alert(fileContent);
+function updateDivContent(fileContent) {
+	var collapsibles = getAllCollapsibles(fileContent);
+	document.getElementById('built').innerHTML = collapsibles;
 }
 
-function getCvAsCollapsibles(file) {
-	alert(file);
-	var reader = new FileReader();
-	reader.onload = function() {
-		if (reader.readyState === FileReader.DONE) {
-			updateDivContent(reader.result);
+function getCvAsCollapsibles() {
+	var fileInput = document.getElementById('fileInput');
+	fileInput.addEventListener('change', function(e) {
+		var file = fileInput.files[0];
+		var textType = /text.*/;
+		if (file.type.match(textType)) {
+			var reader = new FileReader();
+			reader.onload = function(event) {
+				if (reader.readyState === FileReader.DONE) {
+					updateDivContent(event.target.result);
+				}
+			};
+			reader.readAsText(file);
 		}
-	};
-	alert("sup");
-	reader.readAsText(file);
+	});
 }
